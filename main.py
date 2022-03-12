59d84254b05956089f24b7118a620cbcb1a7e3d9
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-03-12 15:41:33 +0300

    plot shows

--- main.py
+++ main.py
@@ -2,77 +2,73 @@ import random as r
 import matplotlib.pyplot as plt
 
 
-def probs(trial):
-    probs1, probs2, probs3 = [], [], []
+def empricalProbs(trial):
+    probs1 = []
+    probs2 = []
+    probs3 = []
     firstCounter, secondCounter, thirdCounter = 0, 0, 0
 
     for _ in range(1, trial + 1):
-        listOfDice = []
-        for i in range(5):
+        numberOfListDice = []
+        for x in range(5):
             dice = r.randint(1, 6)
-            listOfDice.append(dice)
+            numberOfListDice.append(dice)
 
-        if 3 in listOfDice:
+        if 3 in numberOfListDice:
             firstCounter += 1
+
         probs1.append(firstCounter / _)
 
     for _ in range(1, trial + 1):
-        listOfDice = []
-        for i in range(4):
+        numberOfListDice = []
+        for x in range(4):
             dice = r.randint(1, 6)
-            listOfDice.append(dice)
+            numberOfListDice.append(dice)
 
-        if 3 in listOfDice:
+        if 3 in numberOfListDice:
             secondCounter += 1
         probs2.append(secondCounter / _)
 
     for _ in range(1, trial + 1):
-        listOfDice = []
-        for i in range(4):
+        numberOfListDice = []
+        for x in range(4):
             dice = r.randrange(1, 6, 2)
-            listOfDice.append(dice)
-        if 3 in listOfDice:
+            numberOfListDice.append(dice)
+        if 3 in numberOfListDice:
             thirdCounter += 1
         probs3.append(thirdCounter / _)
 
     return firstCounter, secondCounter, thirdCounter, probs1, probs2, probs3
 
 
-def theoreticalSolutionsOutput():
+firstCounter, secondCounter, thirdCounter, probs1, probs2, probs3 = empricalProbs(1000000)
+
+
+def outputTheoreticalProbs():
     print("Theoretical Solutions")
     print(round(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6), 4))
     print(round(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6), 4))
     print(round(1 - (2 / 3) * (2 / 3) * (2 / 3) * (2 / 3), 4))
 
 
-ct1, ct2, ct3, probs1, probs2, probs3 = probs(1000000)
-
-
-def experimentalSolutionsOutput():
+def outputEmprical(probs):
     print("Experimental Solutions")
-    print(ct1 / 1000000)
-    print(ct2 / 1000000)
-    print(ct3 / 1000000)
+    print(round(probs[9], 4), round(probs[49], 4), round(probs[99], 4), round(probs[499], 4), round(probs[999], 4),
+          round(probs[4999], 4), round(probs[9999], 4), round(probs[49999], 4),
+          round(probs[99999], 4), round(probs[499999], 4), round(probs[999999], 4))
 
 
-def plotOutput():
-    plt.plot([probs1[9], probs1[49], probs1[99], probs1[499], probs1[999], probs1[4999], probs1[9999], probs1[49999],
-              probs1[99999], probs1[499999], probs1[999999]])
-
-    plt.show()
-
-    plt.plot([probs2[9], probs2[49], probs2[99], probs2[499], probs2[999], probs2[4999], probs2[9999], probs2[49999],
-
-              probs2[99999], probs2[499999], probs2[999999]])
+def drawGraph(probs):
+    plt.plot([probs[9], probs[49], probs[99], probs[499], probs[999], probs[4999], probs[9999], probs[49999],
+              probs[99999], probs[499999], probs[999999]])
 
     plt.show()
 
-    plt.plot([probs3[9], probs3[49], probs3[99], probs3[499], probs3[999], probs3[4999], probs3[9999], probs3[49999],
 
-              probs3[99999], probs3[499999], probs3[999999]])
-    plt.show()
-
-
-theoreticalSolutionsOutput()
-experimentalSolutionsOutput()
-plotOutput()
+outputTheoreticalProbs()
+outputEmprical(probs1)
+outputEmprical(probs2)
+outputEmprical(probs3)
+drawGraph(probs1)
+drawGraph(probs2)
+drawGraph(probs3)
