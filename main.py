6783b590d8826334f9ead27192df5edf74adaa09
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-03-12 15:05:03 +0300

    plot shows

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def probs(trial):
-    probs1 = []
-    probs2 = []
-    probs3 = []
+    probs1, probs2, probs3 = [], [], []
     firstCounter, secondCounter, thirdCounter = 0, 0, 0
 
     for _ in range(1, trial + 1):
@@ -42,9 +40,9 @@ def probs(trial):
 
 def theoreticalSolutionsOutput():
     print("Theoretical Solutions")
-    print(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6))
-    print(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6))
-    print(1 - (2 / 3) * (2 / 3) * (2 / 3) * (2 / 3))
+    print(round(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6), 4))
+    print(round(1 - (5 / 6) * (5 / 6) * (5 / 6) * (5 / 6), 4))
+    print(round(1 - (2 / 3) * (2 / 3) * (2 / 3) * (2 / 3), 4))
 
 
 ct1, ct2, ct3, probs1, probs2, probs3 = probs(1000000)
@@ -57,21 +55,24 @@ def experimentalSolutionsOutput():
     print(ct3 / 1000000)
 
 
+def plotOutput():
+    plt.plot([probs1[9], probs1[49], probs1[99], probs1[499], probs1[999], probs1[4999], probs1[9999], probs1[49999],
+              probs1[99999], probs1[499999], probs1[999999]])
+
+    plt.show()
+
+    plt.plot([probs2[9], probs2[49], probs2[99], probs2[499], probs2[999], probs2[4999], probs2[9999], probs2[49999],
+
+              probs2[99999], probs2[499999], probs2[999999]])
+
+    plt.show()
+
+    plt.plot([probs3[9], probs3[49], probs3[99], probs3[499], probs3[999], probs3[4999], probs3[9999], probs3[49999],
+
+              probs3[99999], probs3[499999], probs3[999999]])
+    plt.show()
+
+
 theoreticalSolutionsOutput()
 experimentalSolutionsOutput()
-
-plt.plot([probs1[9], probs1[49], probs1[99], probs1[499], probs1[999], probs1[4999], probs1[9999], probs1[49999],
-          probs1[99999], probs1[499999], probs1[999999]])
-
-plt.show()
-
-plt.plot([probs2[9], probs2[49], probs2[99], probs2[499], probs2[999], probs2[4999], probs2[9999], probs2[49999],
-
-          probs2[99999], probs2[499999], probs2[999999]])
-
-plt.show()
-
-plt.plot([probs3[9], probs3[49], probs3[99], probs3[499], probs3[999], probs3[4999], probs3[9999], probs3[49999],
-
-          probs3[99999], probs3[499999], probs3[999999]])
-plt.show()
+plotOutput()
